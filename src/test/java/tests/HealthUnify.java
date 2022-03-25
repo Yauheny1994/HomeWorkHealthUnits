3853f9ca4886706a42d6fa1d708047b1d017937e
Evgeni Makarchuk <makarchuk_evgen@inbox.ru>
2022-03-25 10:51:15 +0300

    removed errors

--- src/test/java/tests/HealthUnify.java
+++ src/test/java/tests/HealthUnify.java
@@ -11,7 +11,7 @@ public class HealthUnify {
     WebDriver driver;
 
     @Test
-    public void checkStarvation() {
+    public void checkCategoryStarvationTest() {
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
         driver = new ChromeDriver();
         driver.get("https://healthunify.com/bmicalculator/");
@@ -22,16 +22,12 @@ public class HealthUnify {
         driver.findElement(By.xpath("//input[@name=\"cc\"]")).click();
         String siUnits = driver.findElement(By.xpath("//input[@name=\"si\"]")).getAttribute("value");
         Assert.assertEquals(siUnits, "3.55");
-        String usUnits = driver.findElement(By.xpath("//input[@name=\"us\"]")).getAttribute("value");
-        Assert.assertEquals(usUnits, "3.61");
-        String ukUnits = driver.findElement(By.xpath("//input[@name=\"uk\"]")).getAttribute("value");
-        Assert.assertEquals(ukUnits, "22.54");
         String category = driver.findElement(By.xpath("//input[@name=\"desc\"]")).getAttribute("value");
         Assert.assertEquals(category, "Your category is Starvation");
     }
 
     @Test
-    public void checkNormal() {
+    public void checkCategoryNormalTest() {
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
         driver = new ChromeDriver();
         driver.get("https://healthunify.com/bmicalculator/");
@@ -41,17 +37,13 @@ public class HealthUnify {
         driver.findElement(By.xpath("//input[@name=\"ht\"]")).sendKeys("175");
         driver.findElement(By.xpath("//input[@name=\"cc\"]")).click();
         String siUnits = driver.findElement(By.xpath("//input[@name=\"si\"]")).getAttribute("value");
-        Assert.assertEquals(siUnits, "18.61");
-        String usUnits = driver.findElement(By.xpath("//input[@name=\"us\"]")).getAttribute("value");
-        Assert.assertEquals(usUnits, "18.93");
-        String ukUnits = driver.findElement(By.xpath("//input[@name=\"uk\"]")).getAttribute("value");
-        Assert.assertEquals(ukUnits, "118.17");
+        Assert.assertEquals(siUnits, "18.61");;
         String category = driver.findElement(By.xpath("//input[@name=\"desc\"]")).getAttribute("value");
         Assert.assertEquals(category, "Your category is Normal");
     }
 
     @Test
-    public void checkOverweight() {
+    public void checkCategoryOverweightTest() {
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
         driver = new ChromeDriver();
         driver.get("https://healthunify.com/bmicalculator/");
@@ -62,17 +54,13 @@ public class HealthUnify {
         driver.findElement(By.xpath("//input[@name=\"cc\"]")).click();
         String siUnits = driver.findElement(By.xpath("//input[@name=\"si\"]")).getAttribute("value");
         Assert.assertEquals(siUnits, "25.1");
-        String usUnits = driver.findElement(By.xpath("//input[@name=\"us\"]")).getAttribute("value");
-        Assert.assertEquals(usUnits, "25.52");
-        String ukUnits = driver.findElement(By.xpath("//input[@name=\"uk\"]")).getAttribute("value");
-        Assert.assertEquals(ukUnits, "159.39");
         String category = driver.findElement(By.xpath("//input[@name=\"desc\"]")).getAttribute("value");
         Assert.assertEquals(category, "Your category is Overweight");
     }
 
 
     @Test
-    public void checkObese() {
+    public void checkCategoryObeseTest() {
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
         driver = new ChromeDriver();
         driver.get("https://healthunify.com/bmicalculator/");
@@ -83,10 +71,6 @@ public class HealthUnify {
         driver.findElement(By.xpath("//input[@name=\"cc\"]")).click();
         String siUnits = driver.findElement(By.xpath("//input[@name=\"si\"]")).getAttribute("value");
         Assert.assertEquals(siUnits, "30.07");
-        String usUnits = driver.findElement(By.xpath("//input[@name=\"us\"]")).getAttribute("value");
-        Assert.assertEquals(usUnits, "30.58");
-        String ukUnits = driver.findElement(By.xpath("//input[@name=\"uk\"]")).getAttribute("value");
-        Assert.assertEquals(ukUnits, "190.94");
         String category = driver.findElement(By.xpath("//input[@name=\"desc\"]")).getAttribute("value");
         Assert.assertEquals(category, "Your category is Obese");
     }
